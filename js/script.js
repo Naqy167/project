178c253d192725df7c60a54ae7df543bc2f7d1d8
Ivan Popov <popovivan.seredka@yandex.ru>
2022-08-08 14:09:38 +0300

    add script

--- js/script.js
+++ js/script.js
@@ -1,50 +1,18 @@
-"use strict"
-let a = [];
-a.push(1, 2, 3);
-console.log(a);
+"use strict";
 
-let points = [
-    {x:0, y:0},
-    {x:1, y:1}
-];
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-
-points.dist = function() {
-    let p1 = this[0];
-    let p2 = this[1];
-    let a = p2.x - p1.x;
-    let b = p2.y - p1.y;
-    return Math.sqrt(a*a + b*b);
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    privat: false
 };
 
-console.log(points.dist());
+let simpleOfFilm = prompt("Один из просмотренных фильмов?", ""),
+    estimationOfFilm = prompt("На сколько оцените его?", "");
 
-let arr = [1, 5, 7, -1]
-const sum = 6;
+personalMovieDB.movies[simpleOfFilm] = estimationOfFilm;
 
-arr.getRes = function(p1, p2, p3, p4){
-    p1 = this[0];
-    p2 = this[1];
-    p3 = this[2];
-    p4 = this[3];
-
-}
-// alert(arr);
-// const result = confirm("Are you here&");
-// console.log(result);
-
-// const answer = prompt("Вам есть 18?", "18");
-// console.log(answer);
-
-// const answers = [];
-
-// answers[0] = prompt("Ваше имя?", "");
-// answers[1] = prompt("Где вы живете?", "");
-// answers[2] = prompt("Сколько вам лет?", "");
-
-// console.log(answers);
-
-// document.write(answers);
-
-const user = 'Ivan';
-alert(`привет, ${user}`);
+console.log(personalMovieDB);
